d351279e817613d9d19589e4678fd86aaf8708ba
PeykoGeorgiev <peyko92gm@gmail.com>
2023-05-30 23:23:13 +0200

    Аdd views files to Project

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
+const handlebars = require('express-handlebars');
 
-const app = express()
+const app = express();
 
 const PORT = 5000
 
+//handlebars config
+ app.engine('hbs', handlebars.engine({
+     extname: 'hbs',
+ }))
+ app.set('view engine', 'hbs')
+ app.set('views', 'src/views')
+
 app.get('/', (req, res) =>{
-    res.send('Hello from express')
+    res.render('home')
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
