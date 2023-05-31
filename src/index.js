934104ddd1ad8e878bb60b6fcf63dcb4d313498c
PeykoGeorgiev <peyko92gm@gmail.com>
2023-05-31 13:20:13 +0200

    Add static assets to Project

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express')
 const handlebars = require('express-handlebars');
+const path = require('path')
 
 const app = express();
 
 const PORT = 5000
+//EXPRESS CONFIG
+app.use(express.static(path.resolve(__dirname, 'public')))
+//app.use(express.static(path.resolve(__dirname, '../public')))
+//app.use(express.static(path.resolve(__dirname, '../public'))); // to be able to use static files from public Need to setup Path module
+
+
 
 //handlebars config
  app.engine('hbs', handlebars.engine({
@@ -12,8 +19,9 @@ const PORT = 5000
  app.set('view engine', 'hbs')
  app.set('views', 'src/views')
 
+ //router
 app.get('/', (req, res) =>{
-    res.render('home')
+    res.render('index')
 });
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}...`))
